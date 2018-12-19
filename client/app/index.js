06b6c16946fa4ebbfe114cbf8af8d138239d1936
Bruno Campos <brunosopmac@gmail.com>
2018-12-19 00:08:54 -0200

    Avançando no cap. 1

---
+++ client/app/index.js
@@ -0,0 +1,32 @@
+// campos do form
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+];
+
+console.log(campos);
+
+// buscando tbody
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+
+    event.preventDefault();
+   
+    var tr = document.createElement('tr');
+
+    campos.forEach(function(campo) {
+        var td = document.createElement('td');
+
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    var tdVolume = document.createElement('td');
+    
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+});
