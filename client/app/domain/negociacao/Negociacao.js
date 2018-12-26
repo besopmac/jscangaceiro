b9d78f4847cf7eee2c3f57c6aa3628f0310e5873
Bruno Campos <brunosopmac@gmail.com>
2018-12-26 19:11:28 -0200

    recriando a classe negociacao

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,31 +1,3 @@
 class Negociacao {
-
-    // definindo as propriedades da classe
-    constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-        Object.freeze(this); // congela a instância
-    }
-
-    /*
-     * getter: uma propriedade de leitura que
-     * impede a mudança de valores diretamente
-     * 
-     */
-    get volume() {
-        return this._quantidade * this._valor;
-    }
-
-    get data() {
-        return this._data;
-    }
-
-    get quantidade() {
-        return this._quantidade;
-    }
-
-    get valor() {
-        return this._valor;
-    }
+    
 }
