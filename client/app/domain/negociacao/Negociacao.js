cf7099c48cf10bd218742427aa45ea2ea6578871
Bruno Campos <brunosopmac@gmail.com>
2018-12-26 19:32:33 -0200

    compreendendo o conceito de getters

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,9 +1,33 @@
 class Negociacao {
 
+    /*
+     *  Toda propriedade de uma classe prefixada com um underline
+     *  NÃO deve ser acessada fora dos métodos da própria classe.
+     */
+
     constructor(data, quantidade, valor) {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
+    obtemVolume() {
+        return this._quantidade * this._valor;
+    }
+
+    /*
+     * Para não quebrar a convenção citada acima
+     * são criados os métodos get, a.k.a GETTERS.
+     */
+    getData() {
+        return this._data;
+    }
+
+    getQuantidade() {
+        return this._quantidade;
+    }
+
+    getValor() {
+        return this._valor;
+    }
 }
