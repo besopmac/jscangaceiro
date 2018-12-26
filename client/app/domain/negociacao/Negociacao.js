242e432849393392eba86986e6b59cfb27333634
Bruno Campos <brunosopmac@gmail.com>
2018-12-26 19:55:05 -0200

    compreendendo object assign

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -6,7 +6,7 @@ class Negociacao {
      */
 
     constructor(data, quantidade, valor) {
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
 
@@ -22,7 +22,8 @@ class Negociacao {
      * ser acessados como propriedades.
      */
     get data() {
-        return this._data;
+        // return this._data;
+        return new Date(this._data.getTime());
     }
 
     get quantidade() {
